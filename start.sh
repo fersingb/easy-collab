58a29ee70ce5341f402a5a47b5f25f21d641da98
Boris Fersing <boris@fersing.net>
2020-02-10 14:45:31 -0500

    Updated start script

--- start.sh
+++ start.sh
@@ -7,20 +7,18 @@ fi
 DOMAIN=$1
 IP=$2
 
-
-### BIND CONFIG ###
-
-docker run -d --rm  \
+docker run -d  \
 	--name=easy-collab \
 	-e PUID=$(id -u) \
 	-e PGID=$(id -g) \
 	-e DOMAIN=$DOMAIN \
 	-e IP=$IP \
-	-e STAGING=true \
+	-e STAGING=false \
 	-e DEFAULT_DHPARAMS=true \
 	-p 443:443 \
 	-p 80:80 `#optional` \
 	-p $IP:53:53 \
 	-p $IP:53:53/udp \
 	-v $(pwd)/data:/config \
+	--restart unless-stopped \
 	fersingb/easy-collab
